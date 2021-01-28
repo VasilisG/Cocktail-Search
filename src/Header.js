4c0f98d5f51c952144523cdda2d377fadb3e3398
Vasilis <vasilisgoutz08@gmail.com>
2021-01-29 01:47:12 +0200

    Installed react-router-dom and set routes.

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
+import Cocktail from './Pages/Cocktail';
+import Ingredient from './Pages/Ingredient';
 
 class Header extends Component {
 
@@ -8,18 +11,31 @@ class Header extends Component {
                 <div className="title-container">
                     <h1>Cocktail Junkie</h1>
                 </div>
-                <div class="navigation-container">
-                    <nav className="navigation">
-                        <ul className="nav-items">
-                            <li className="nav-item">
-                                <a href="\cocktail">Cocktails</a>
-                            </li>
-                            <li className="nav-item">
-                                <a href="\ingredient">Ingredients</a>
-                            </li>
-                        </ul>                
-                    </nav>
-                </div>
+                <Router>
+                    <div class="navigation-container">
+                        <nav className="navigation">
+                            <ul className="nav-items">
+                                <li className="nav-item">
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/cocktails">Cocktails</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/ingredients">Ingredients</Link>
+                                </li>
+                            </ul>                
+                        </nav>
+                        <Switch>
+                            <Route path="/cocktails">
+                                <Cocktail/>
+                            </Route>
+                            <Route path="/ingredients">
+                                <Ingredient/>
+                            </Route>
+                        </Switch>
+                    </div>
+                </Router>
             </header>
         )
     }
