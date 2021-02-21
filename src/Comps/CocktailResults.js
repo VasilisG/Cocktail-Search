126fab1d9a19c7649359bb0529b4d1dedd2e5773
Vasilis <vasilisgoutz08@gmail.com>
2021-02-21 23:35:55 +0200

    Created cocktail results component.

---
+++ src/Comps/CocktailResults.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class CocktailResults extends Component {
+
+    render() {
+        return (
+            <div class="cocktail-items-container">
+                <ul class="cocktail-items">
+
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default CocktailResults;
